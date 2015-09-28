7bb4577773d8a3e789608316a009af87109964eb
Hoang Dang Trung <trunghd.99bn@gmail.com>
2015-09-28 10:47:07 +0700

    ok

---
+++ TableViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewDemo
+//
+//  Created by hoang dang trung on 9/28/15.
+//  Copyright © 2015 hoang dang trung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewDemo/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  TableViewDemo
+//
+//  Created by hoang dang trung on 9/28/15.
+//  Copyright © 2015 hoang dang trung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
